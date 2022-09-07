e9684c4ef19001eb6fe533242344c95602b9a740
RIVAILDO <80759812+F3RREIRA@users.noreply.github.com>
2022-09-06 21:39:40 -0300

    Add files via upload

---
+++ Aulas/06-09-22/VariaveisCosntantes.js
@@ -0,0 +1,12 @@
+/*(function(){
+    var nome = 'Johnata santicioli'
+    console.log(nome);
+})()
+console.log(nome);*/
+
+globalThis.nomeGlobal = 'Rivaildo'
+if(true){
+    globalThis.nomeGlobal2 = 'Natasha'
+    console.log(nomeGlobal);
+}
+console.log(nomeGlobal2);
