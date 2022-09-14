e9ca914d41bf21e07f80108ca6cc4750c5469cd3
RIVAILDO <80759812+F3RREIRA@users.noreply.github.com>
2022-09-13 22:21:36 -0300

    Add files via upload

---
+++ Aulas/13-09-22/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const 13-09-22 = () => {
+  return <View />;
+}
+
+export default 13-09-22;
