e71d75ac4f8e0912bc735b974b63f21494acb3a5
RAPHAEL <80759812+C4PISTRANO@users.noreply.github.com>
2023-02-28 21:49:44 -0300

    Add files via upload

---
+++ Aulas/28-02-2023/Site/script/1.js
@@ -0,0 +1,13 @@
+var num = parseInt (prompt("Informe um número de 1 a 10:"))
+	
+	function()
+	{
+		if (num % 2 === 0) {
+		document.write("par")
+		} 
+		else {
+		document.write("impar")
+		}
+	}
+	
+	return
---
+++ Aulas/28-02-2023/Site/script/2.js
@@ -0,0 +1,10 @@
+function incremento(){
+	var num = parseInt (prompt("Informe um número de 1 a 10:"))
+	var i=0
+	
+	while (i < num) {
+		i++;
+		x +=n:
+		
+	}
+}
