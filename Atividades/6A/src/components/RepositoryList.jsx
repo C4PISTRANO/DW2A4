4c3858f0bd2b503bcdc7d4e56f623fccd44e08e3
RAPHAEL <80759812+C4PISTRANO@users.noreply.github.com>
2023-03-09 22:33:00 -0300

    Update RepositoryList.jsx

--- Atividades/6A/src/components/RepositoryList.jsx
+++ Atividades/6A/src/components/RepositoryList.jsx
@@ -4,7 +4,7 @@ import { RepositoryItem } from "./RepositoryItem"
 const repository = {
     name: "Atividade",
     description: "Descrição da Atividade",
-    link: "https://github.com/F3RREIRA/DW2A4/tree/main/Atividades/6A"
+    link: "https://github.com/C4PISTRANO/DW2A4/tree/main/Atividades/6A"
 }
 
 export function RepositoryList(){
